5d93584434c01936682b01fbfa77eef50a3d2d77
Arpitha Bhat <arpithabht12@gmail.com>
2021-10-07 23:04:22 +0530

    ADDED: restricts the alphabets at beginning of pincode

--- pincode.js
+++ pincode.js
@@ -1,11 +1,18 @@
 function pinCodeValidator(pincode){
-    let pinCodeRegex = RegExp("[0-9]{6}");
+    let pinCodeRegex = RegExp("^[0-9]{6}");
     if(pinCodeRegex.test(pincode))
         console.log("Valid");
     else throw 'Pincode is not valid';
 }
-try{
-    pinCodeValidator(500231);
-}catch(e){
-    console.error(e);
-}
+
+let pinCodeArr = new Array();
+pinCodeArr.push("577432");
+pinCodeArr.push("56783");
+pinCodeArr.push("B678123");
+pinCodeArr.forEach(element => {
+    try{
+        pinCodeValidator(element);
+    }catch(e){
+        console.error(e);
+    }
+});
