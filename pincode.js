d6a11cb8a95664e02cb7de2e0900c1dacf99b7b2
Arpitha Bhat <arpithabht12@gmail.com>
2021-10-07 23:08:52 +0530

    ADDED: pincode with white spaces in the middle is considered valid

--- pincode.js
+++ pincode.js
@@ -1,5 +1,5 @@
 function pinCodeValidator(pincode){
-    let pinCodeRegex = RegExp("^[0-9]{6}$");
+    let pinCodeRegex = RegExp("^[0-9]{3}[\\s]?[0-9]{3}$");
     if(pinCodeRegex.test(pincode))
         console.log("Valid");
     else throw 'Pincode is not valid';
@@ -10,6 +10,7 @@ pinCodeArr.push("577432");
 pinCodeArr.push("56783");
 pinCodeArr.push("B678123");
 pinCodeArr.push("57743@");
+pinCodeArr.push("577 432");
 pinCodeArr.forEach(element => {
     try{
         pinCodeValidator(element);
